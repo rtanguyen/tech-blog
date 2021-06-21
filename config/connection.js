35c02394ffa64df18075ac87e47d54bb7ea11285
rtanguyen <rtanguyen@gmail.com>
2021-06-21 18:23:42 -0500

    initial files

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//import sequelize constructor from library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+//create connection to our database, pass in mysql info
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'secretstuff',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db:sequelize
+    })
+}
+
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(session(sess));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
