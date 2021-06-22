31662da77caa9ce3ca574238c82cef1ef75205a4
rtanguyen <rtanguyen@gmail.com>
2021-06-22 04:48:17 -0500

    api rotues and dashboard/home routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,78 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+
+//GET /api/users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData)
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+//POST /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        // email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//LOGIN
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({message: 'No user with that username'});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+           if(!validPassword) {
+               res.status(400).json({message: 'incorrect password'});
+               return;
+           }
+           req.session.save(() => {
+               req.session.user_id = dbUserData.id;
+               req.session.username = dbUserData.username;
+               req.session.loggedIn = true;
+               
+               res.json({user: dbUserData, message: 'you are logged in'});
+           });
+    });
+});
+
+//LOGOUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        order: [['created_at', 'DESC']],
+        attributes: ['id', 
+        'title', 
+        'content', 
+        'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', {
+            posts, 
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//edit post
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        order: [['created_at', 'DESC']],
+        attributes: ['id', 
+        'title', 
+        'content', 
+        'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'no post found with this id' });
+            return;
+        }
+        //serialize the data
+        const post = dbPostData.get({ plain: true });
+        //pass data to template
+        res.render('edit-post', { 
+            post,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
