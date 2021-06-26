b200a8b5007bd0e0866e0904c984f9da4d84138b
rtanguyen <rtanguyen@gmail.com>
2021-06-26 11:18:10 -0500

    fix heroku?

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,26 +1,44 @@
+let image_url = "";
+
 async function signupFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+
   
     if (username && password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
-          password
+          password,
+          // image_url
         }),
         headers: { 'Content-Type': 'application/json' }
       });
       //check response status
       if (response.ok) {
         console.log('success');
-        document.location.replace('/dashboard');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
     }
   }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+// var myWidget = cloudinary.createUploadWidget({
+//   cloudName: 'dipwtij2r', 
+//   uploadPreset: 'miso-hungry'}, (error, result) => { 
+//     if (!error && result && result.event === "success") { 
+//       console.log('Done! Here is the image info: ', result.info); 
+//       image_url = response.info.secure_url
+//     }
+//   }
+// )
+
+// document.getElementById("upload_widget").addEventListener("click", function(){
+//     myWidget.open();
+//   }, false);
